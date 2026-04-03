14c8bd312b49acbc1bc595c36c876c8779281919
Holdthat <joemori@morirealty.com>
2026-04-03 19:21:43 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>STRInvestCalc</h1>
+      <p>Investment Analysis Tool</p>
+    </div>
+  );
+}
+
+export default App;
